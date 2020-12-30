0c70138f885d4dd869ddc10bc67eca3af23a985b
Nevena <44-2018@ftn.edu.rs>
2020-12-31 00:55:31 +0100

    Kreirana baza i tabele, login forma i dodata forma  FormStudents

--- Project/PresentationLayer/PresentationLayer/Form1.Designer.cs
+++ Project/PresentationLayer/PresentationLayer/Form1.Designer.cs
@@ -29,13 +29,118 @@ namespace PresentationLayer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelUserName = new System.Windows.Forms.Label();
+            this.textBoxUserName = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.checkBoxHide = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Location = new System.Drawing.Point(137, 58);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(40, 13);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "LOGIN";
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Location = new System.Drawing.Point(358, 173);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(58, 13);
+            this.labelUserName.TabIndex = 1;
+            this.labelUserName.Text = "User name";
+            // 
+            // textBoxUserName
+            // 
+            this.textBoxUserName.Location = new System.Drawing.Point(438, 173);
+            this.textBoxUserName.Name = "textBoxUserName";
+            this.textBoxUserName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxUserName.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(438, 227);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 20);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(358, 227);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(53, 13);
+            this.labelPassword.TabIndex = 3;
+            this.labelPassword.Text = "Password";
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(340, 301);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Text = "button1";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(483, 301);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(75, 23);
+            this.buttonLogin.TabIndex = 6;
+            this.buttonLogin.Text = "button2";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // checkBoxHide
+            // 
+            this.checkBoxHide.AutoSize = true;
+            this.checkBoxHide.Location = new System.Drawing.Point(601, 227);
+            this.checkBoxHide.Name = "checkBoxHide";
+            this.checkBoxHide.Size = new System.Drawing.Size(48, 17);
+            this.checkBoxHide.TabIndex = 7;
+            this.checkBoxHide.Text = "Hide";
+            this.checkBoxHide.UseVisualStyleBackColor = true;
+            this.checkBoxHide.CheckedChanged += new System.EventHandler(this.checkBoxHide_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(877, 450);
+            this.Controls.Add(this.checkBoxHide);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxUserName);
+            this.Controls.Add(this.labelUserName);
+            this.Controls.Add(this.labelLogin);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelUserName;
+        private System.Windows.Forms.TextBox textBoxUserName;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.CheckBox checkBoxHide;
     }
 }
 
--- Project/PresentationLayer/PresentationLayer/Form1.cs
+++ Project/PresentationLayer/PresentationLayer/Form1.cs
@@ -16,5 +16,44 @@ namespace PresentationLayer
         {
             InitializeComponent();
         }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string userName, pass;
+            userName = textBoxUserName.Text;
+            pass = textBoxPassword.Text;
+            if ((userName == "administrator1" && pass == "password1") || (userName == "administrator2" && pass == "password2") ||
+                (userName == "administrator3" && pass == "password3"))
+            {
+                MessageBox.Show("Success");
+                FormStudents formStudents = new FormStudents();
+                formStudents.Show();
+
+            }
+            else
+            {
+                MessageBox.Show("Error Try again!");
+
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+
+        }
+
+        private void checkBoxHide_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBoxHide.Checked)
+            {
+                textBoxPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textBoxPassword.UseSystemPasswordChar = true;
+
+            }
+        }
     }
 }
