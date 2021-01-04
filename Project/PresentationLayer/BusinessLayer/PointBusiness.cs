311976dcadc1cabbc1f416ea8af6c651abca431f
ISBI <isakbiberovic@gmail.com>
2021-01-04 22:59:53 +0100

    Pozvane metode u biznis sloju.

---
+++ Project/PresentationLayer/BusinessLayer/PointBusiness.cs
@@ -0,0 +1,62 @@
+﻿using DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class PointBusiness
+    {
+
+        public readonly PointRepository pointRepository;
+
+        public StudentBusiness()
+        {
+            
+            this.pointRepository = new PointRepository();
+            
+        }
+
+        public List<PreexeminationPoint> GetPoints()
+        {
+            return this.pointRepository.GetPoint();
+        }
+
+        public bool InsertPoint(PreexeminationPoint p)
+        {
+            if (this.pointRepository.Insert(p) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdatePoint(PreexeminationPoint p)
+        {
+            if (this.pointRepository.Update(p) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool DeletePoint(PreexeminationPoint p)
+        {
+            if (this.pointRepository.Delete(p) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+       
+        public bool DeletePoint1(Point p, int idNew)
+        {
+            if (this.pointtRepository.Delete1(p, idNew) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Project/PresentationLayer/BusinessLayer/StudentBusiness.cs
@@ -0,0 +1,61 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class StudentBusiness
+    {
+
+        public readonly StudentRepository studentRepository;
+
+        public StudentBusiness()
+        {
+            this.studentRepository = new StudentRepository();
+        }
+
+        public List<Student> GetStudents()
+        {
+            return this.studentRepository.GetStudent();
+        }
+        public bool InsertStudent(Student s)
+        {
+            if (this.studentRepository.Insert(s) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool UpdateStudent(Student s)
+        {
+            if (this.studentRepository.Update(s) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool DeleteStudent(Student s)
+        {
+            if (this.studentRepository.Delete(s) > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        public bool DeleteStudent1(Student s, int idNew)
+        {
+            if (this.studentRepository.Delete1(s, idNew) > 0)
+            {
+                return true;
+            }
+            return false;
+
+
+        }
+    }
+}
